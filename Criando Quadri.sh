911861e86fbf5dd099b77977af19fa9377e5e77d
Vinícius Nicassio Ferreira <vinicius.nicassio@gmail.com>
2015-08-05 17:20:21 -0300

    Delete Criando Quadri.sh

--- Criando Quadri.sh
+++
@@ -1,6 +0,0 @@
-echo "# Quadricoptero" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/viniciusnicassio/Quadricoptero.git
-git push -u origin master
