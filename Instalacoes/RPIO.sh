270b26f6b0d3e297d1e7a7c2d2573154aecf2ce1
SFotakos <s.fotakos@gmail.com>
2015-08-26 16:52:33 -0300

    Criado pasta instalacoes para futuros .sh

---
+++ Instalacoes/RPIO.sh
@@ -0,0 +1,7 @@
+cd ~/
+sudo git clone https://github.com/metachris/RPIO
+sudo apt-get install python-setuptools
+sudo easy_install RPIO
+sudo apt-get install python-dev
+#sudo apt-get install python-devel
+sudo apt-get install build-essential
