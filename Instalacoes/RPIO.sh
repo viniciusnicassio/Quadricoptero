b15226c0915c843f816c1cd49a6ea14129d4a413
SFotakos <s.fotakos@gmail.com>
2015-09-01 23:51:35 -0300

    Alteração nos arquivos .sh /n Criado arquivo de instalação do zero.

--- Instalacoes/RPIO.sh
+++
@@ -1,7 +0,0 @@
-cd ~/
-sudo git clone https://github.com/metachris/RPIO
-sudo apt-get install python-setuptools
-sudo easy_install RPIO
-sudo apt-get install python-dev
-#sudo apt-get install python-devel
-sudo apt-get install build-essential
--- sh/ConfigWifi.sh
+++
@@ -1,5 +0,0 @@
-sudo ifconfig wlan0 up
-sudo iwconfig wlan0 essid drone
-sudo dhclient wlan0
-iwconfig wlan0
-ifconfig wlan0
